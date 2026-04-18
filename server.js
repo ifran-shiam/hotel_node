6e15f8e958e9088fdef042c87cbfa350703fff7f
Ifran <ifranhasanshiam@gmail.com>
2026-04-18 22:16:23 +0600

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+
 
 
 
@@ -25,6 +28,6 @@ app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on http://localhost:3000')
 });
